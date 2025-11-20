61985ce7af74c2a92ded319476b89ff45344fd77
Ryan smith <ryansmith@Ryans-Mac-Studio.local>
2025-11-20 13:34:19 -0800

    Simplify Prisma client

--- productivity-hub/lib/prisma.ts
+++ productivity-hub/lib/prisma.ts
@@ -2,14 +2,6 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        datasources: {
-            db: {
-                url: process.env.POSTGRES_URL_NON_POOLING,
-            },
-        },
-    });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
